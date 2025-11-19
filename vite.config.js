27c1f9c03404324f6d891eecdb06e62254a20ac6
Jon Deuf <roulibali@gmail.com>
2025-11-19 11:54:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/vol-deaflympics/", // nécessaire pour GitHub Pages
   plugins: [react()],
-})
+});
+
